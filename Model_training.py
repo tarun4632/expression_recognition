97fb362f0386cbb6cffea88718b42fb1b0af6934
tarun jain <99501770+tarun4632@users.noreply.github.com>
2024-02-10 01:01:13 +0530

    Update Model_training.py

--- Model_training.py
+++ Model_training.py
@@ -53,12 +53,6 @@ recog_model = tf.keras.models.Sequential([
         tf.keras.layers.Dropout(0.2),
 
 
-        tf.keras.layers.Conv2D(256,(3,3),activation='relu', input_shape = (48,48,1)),
-        tf.keras.layers.MaxPool2D(2,2),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dropout(0.2),
-
-
 
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation = 'relu'),
